0c89dedd42842834dc98c2f50625c7f97b64f362
alanw777 <wpunity@163.com>
2019-11-28 10:40:50 +0800

    Dijkstra Double Stack Algorithm

--- AlgorithmsWithCs/Program.cs
+++ AlgorithmsWithCs/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using AlgorithmsWithCs.Misc;
+using AlgorithmsWithCs.StackAndQueue;
 
 namespace AlgorithmsWithCs
 {
@@ -12,6 +15,45 @@ namespace AlgorithmsWithCs
             Utils.Log(rv.ToString());
             rv = ThreeSum.BinarySearchFind(a);
             Utils.Log(rv.ToString());
+            //(((1+(2*3))-1)/2)
+            Utils.Log(DijkstraDoubleStackAlgorithm.Calculate(new String[] {"(", "(", "(","1","+","(","2","*","3",")",")","-","1",")","/","2",")"}).ToString());
+
+        }
+
+        class MyClass : IEnumerable
+        {
+            private int[] a;
+            public IEnumerator GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+
+            class MyClassEnumerator:IEnumerator
+            {
+                public bool MoveNext()
+                {
+                    throw new NotImplementedException();
+                }
+
+                public void Reset()
+                {
+                    throw new NotImplementedException();
+                }
+
+                public MyClass Current { get; }
+
+                object IEnumerator.Current => Current;
+
+                public void Dispose()
+                {
+                    throw new NotImplementedException();
+                }
+            }
         }
     }
 }
